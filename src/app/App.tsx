bf343f53c7bd0278690bddd2976933df26511845
= <=>
2024-02-18 15:03:13 +0300

    ff

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,8 +5,12 @@ import { App } from 'antd';
 export function MyApp() {
   return (
     <App>
-      <Helmet title={'ЛК Грейси'}>
+      <Helmet title={'Личный Кабинт Gracey'}>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta
+          name="description"
+          content="Закажи сиделку в один клик"
+        />
       </Helmet>
       <AppRoutes />
     </App>
