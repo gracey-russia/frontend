1b3c7e913d8ca54e72d437a441d48ab28b202378
= <=>
2024-05-19 00:47:09 +0300

    fix

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 
 // export const BACKEND_URL = 'http://194.146.242.64:4002/';
 // export const BACKEND_URL = 'http://127.0.0.1:8000/';
-export const BACKEND_URL = 'https://lk.gracey.ru:4002/'
+export const BACKEND_URL = 'https://lk.gracey.ru:2053/'
 export const BACKEND_MEDIA_PORT = '8000';
 
 export const API_URL = BACKEND_URL + 'api/';
